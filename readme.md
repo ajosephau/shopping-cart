3c247ec9018e408d67129e12c719c95c824208cd
Anthony Joseph <ajosephau@users.noreply.github.com>
2022-09-18 17:03:34 +1000

    fix build issues

--- readme.md
+++ readme.md
@@ -4,11 +4,6 @@
 
 Version number: 72cf4fe47f85c39779267d0ecee07655a354e623
 
-## The problem
-As you work through the steps, you’ll create code to allow a user to add products to a shopping cart, calculate the total price, and then the sales tax for the items contained in the cart. As mentioned previously, we are not looking for a command line or a web application, so please just use your tests to drive the code (e.g. class libraries).
- 
-Please Note: All totals should be rounded up to 2 decimal places, i.e. 0.565 should result in 0.57 but 0.5649 should result in 0.56. This is basically the same rounding you learned at school, but you can follow this link if you really want more details.
-
 # Installation instructions
 
 - Install a [Python 3.10 interpreter](https://www.python.org/downloads/).
@@ -19,10 +14,10 @@ Please Note: All totals should be rounded up to 2 decimal places, i.e. 0.565 sho
 platform win32 -- Python 3.10.4, pytest-7.1.2, pluggy-1.0.0
 rootdir: C:\Users\antho\PycharmProjects\shopping_cart
 plugins: cov-3.0.0
-collected 3 items                                                                                                                
+collected 3 items
 
 test_step_1.py .                                                                                                           [ 33%]
-test_step_2.py .                                                                                                           [ 66%] 
+test_step_2.py .                                                                                                           [ 66%]
 test_step_3.py .                                                                                                           [100%]
 
 ---------- coverage: platform win32, python 3.10.4-final-0 -----------
@@ -36,10 +31,19 @@ test_step_3.py      28      1    96%
 TOTAL               96      3    97%
 
 
-======================================================= 3 passed in 0.29s ======================================================= 
+======================================================= 3 passed in 0.29s =======================================================
 ```
 
+# Quality assurance
+
+I used `pre-commit` to run the linting and QA checks. If you want to run this manually, you can run `pre-commit run --all-files`. This command is useful if you want to run all the QA functions at once. <br /> But if you want to run it automatically before commits then run `pre-commit install` [or see the pre-commit documentation for other integrations](https://pre-commit.com/#3-install-the-git-hook-scripts).
+
 # Problem statement
+
+As you work through the steps, you’ll create code to allow a user to add products to a shopping cart, calculate the total price, and then the sales tax for the items contained in the cart. As mentioned previously, we are not looking for a command line or a web application, so please just use your tests to drive the code (e.g. class libraries).
+
+Please Note: All totals should be rounded up to 2 decimal places, i.e. 0.565 should result in 0.57 but 0.5649 should result in 0.56. This is basically the same rounding you learned at school, but you can follow this link if you really want more details.
+
 ## Assumptions
 
 - Any sales tax applies to the whole shopping cart, and there aren't tax-exempt items in a shopping cart.
@@ -57,7 +61,7 @@ When:
 Then:
 - The shopping cart should contain 5 Dove Soaps each with a unit price of 39.99
 - And the shopping cart’s total price should equal 199.95
-  
+
 ## Step 2
 
 Add additional products of the same type to the shopping cart.
@@ -92,8 +96,8 @@ Then:
 - And the shopping cart should contain 2 Axe Deos each with a unit price of 99.99
 - And the total sales tax amount for the shopping cart should equal 35.00
 - And the shopping cart’s total price should equal 314.96
- 
- 
+
+
 ## What we are looking for:
 - Test Coverage: The solution should be developed “test-first” and should have excellent unit tests and test coverage. It will be hard to pass this exercise if we can’t reasonably believe the solution was developed “test-first” following TDD.
 - Build file: Please provide an automated build file that compiles your code and runs the tests. For java submissions, a Gradle or Maven build file is ideal. Submissions without an automated build will not be accepted.
--- test_step_3.py
+++ test_step_3.py
@@ -1,10 +1,10 @@
+# -*- coding: utf-8 -*-
 import unittest
 
-from models import ShoppingCart, Product
+from models import Product, ShoppingCart
 
 
 class TestStepThree(unittest.TestCase):
-
     def test_main_scenario(self):
         # Given:
         # An empty shopping cart
@@ -12,12 +12,18 @@ class TestStepThree(unittest.TestCase):
         self.assertEqual(shopping_cart.num_items, 0, "Shopping cart is not empty")
         # And a product, Dove Soap with a unit price of 39.99
         product_dove_soap = Product(name="Dove Soap", unit_price_excl_tax=39.99)
-        self.assertEqual(product_dove_soap.name, "Dove Soap", "Product name is incorrect")
-        self.assertEqual(product_dove_soap.unit_price_excl_tax, 39.99, "Product price is incorrect")
+        self.assertEqual(
+            product_dove_soap.name, "Dove Soap", "Product name is incorrect"
+        )
+        self.assertEqual(
+            product_dove_soap.unit_price_excl_tax, 39.99, "Product price is incorrect"
+        )
         # And another product, Axe Deo with a unit price of 99.99
         product_axe_deo = Product(name="Axe Deo", unit_price_excl_tax=99.99)
         self.assertEqual(product_axe_deo.name, "Axe Deo", "Product name is incorrect")
-        self.assertEqual(product_axe_deo.unit_price_excl_tax, 99.99, "Product price is incorrect")
+        self.assertEqual(
+            product_axe_deo.unit_price_excl_tax, 99.99, "Product price is incorrect"
+        )
         # And a sales tax rate of 12.5%
         shopping_cart.sales_tax_rate = 0.125
 
@@ -28,22 +34,54 @@ class TestStepThree(unittest.TestCase):
         shopping_cart.add_item(product=product_axe_deo, quantity=2)
 
         # Then:
-        self.assertEqual(shopping_cart.num_items, 4, "Incorrect number of items in cart")
-        self.assertEqual(len(shopping_cart.products.keys()), 2, "Incorrect number of cart products")
-        self.assertDictEqual(shopping_cart.products, {product_dove_soap: 2, product_axe_deo: 2}, "Cart is incorrect")
+        self.assertEqual(
+            shopping_cart.num_items, 4, "Incorrect number of items in cart"
+        )
+        self.assertEqual(
+            len(shopping_cart.products.keys()), 2, "Incorrect number of cart products"
+        )
+        self.assertDictEqual(
+            shopping_cart.products,
+            {product_dove_soap: 2, product_axe_deo: 2},
+            "Cart is incorrect",
+        )
         # The shopping cart should contain 2 Dove Soaps each with a unit price of 39.99
-        self.assertEqual(shopping_cart.products[product_dove_soap], 2, "Cart has incorrect number of Dove Soap items")
-        self.assertEqual(list(shopping_cart.products.keys())[0].name, "Dove Soap", "Cart item name incorrect")
-        self.assertEqual(list(shopping_cart.products.keys())[0].unit_price_excl_tax, 39.99, "Cart unit price incorrect")
+        self.assertEqual(
+            shopping_cart.products[product_dove_soap],
+            2,
+            "Cart has incorrect number of Dove Soap items",
+        )
+        self.assertEqual(
+            list(shopping_cart.products.keys())[0].name,
+            "Dove Soap",
+            "Cart item name incorrect",
+        )
+        self.assertEqual(
+            list(shopping_cart.products.keys())[0].unit_price_excl_tax,
+            39.99,
+            "Cart unit price incorrect",
+        )
         # And the shopping cart should contain 2 Axe Deos each with a unit price of 99.99
-        self.assertEqual(shopping_cart.products[product_axe_deo], 2, "Cart has incorrect number of Axe Deo items")
-        self.assertEqual(list(shopping_cart.products.keys())[1].name, "Axe Deo", "Cart item name incorrect")
-        self.assertEqual(list(shopping_cart.products.keys())[1].unit_price_excl_tax, 99.99, "Cart unit price incorrect")
+        self.assertEqual(
+            shopping_cart.products[product_axe_deo],
+            2,
+            "Cart has incorrect number of Axe Deo items",
+        )
+        self.assertEqual(
+            list(shopping_cart.products.keys())[1].name,
+            "Axe Deo",
+            "Cart item name incorrect",
+        )
+        self.assertEqual(
+            list(shopping_cart.products.keys())[1].unit_price_excl_tax,
+            99.99,
+            "Cart unit price incorrect",
+        )
         # And the total sales tax amount for the shopping cart should equal 35.00
         self.assertEqual(shopping_cart.total_sales_tax, 35.00, "Incorrect sales tax")
         # And the shopping cart’s total price should equal 314.96
         self.assertEqual(shopping_cart.total, 314.96, "Incorrect cart total")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
